052e8b28499f736a26297ea278bb2a2c53e2ed20
grantshandy <grantshandy@gmail.com>
2021-11-11 15:29:24 -0700

    refactor

--- index.js
+++ index.js
@@ -3,15 +3,56 @@ const client_id = "2ed0e6e8b06842fb854cb15e1690a7b5";
 const redirect_uri = "http://localhost:5500/index.html";
 const scopes = "user-follow-read";
 
-// Check if our page was passed a token...
-const token = window.location.hash.substr(1).split('&')[0].split("=")[1]
-if (token) {
-  // If we already have the token delete the authentication button
-  document.getElementById("authenticateButton").remove();
-  console.log(token);
-} else {
-  // If we don't have the token, add an authentication button to login to spotify.
-  document.getElementById("authenticateButton").onclick = function() {
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
-  };
+(async () => {
+  const token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+
+  // Check if our page was passed a token...
+  if (token) {
+    // Take our artists and print them.
+    var artists = await getFollowingArtists(token);
+    artists.forEach(function(artist) {
+      document.getElementById("content").innerHTML += `<p><a href='${artist.external_urls.spotify}'>${artist.name}</a> id=${artist.id}</p>`;
+    })
+
+  } else {
+    // If not then add an authenticate button.
+    var authenticateButton = document.createElement("input");
+    authenticateButton.type = "button";
+    authenticateButton.value = "Login To Spotify";
+    authenticateButton.onclick = function() {
+      window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
+    };
+    document.body.appendChild(authenticateButton);
+  }
+})();
+
+async function getFollowingArtists(token) {
+  var total;
+  var artists;
+
+  // Get number of artists so we know how much to download.
+  // This is redundant but spotify's api is kinda dumb here.
+  await fetch('https://api.spotify.com/v1/me/following?type=artist', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    total = data.artists.total;
+  })
+
+  // Get our artists and print each of them.
+  await fetch(`https://api.spotify.com/v1/me/following?type=artist&limit=${total}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+    artists = data.artists.items
+  });
+
+  return artists;
 }
