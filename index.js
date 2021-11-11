5f3c16612d6b7c901cb2b552f9ebb8ece314d1dd
grantshandy <grantshandy@gmail.com>
2021-11-11 13:08:37 -0700

    fix spotify login

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-const authenticateButton = document.getElementById("authenticateButton");
+// Some settings for getting into spotify...
 const client_id = "2ed0e6e8b06842fb854cb15e1690a7b5";
 const redirect_uri = "http://localhost:5500/index.html";
-const scopes = "user-top-read";
+const scopes = "user-follow-read";
 
+// Check if our page was passed a token...
 const token = window.location.hash.substr(1).split('&')[0].split("=")[1]
-
 if (token) {
-  console.log(token)
-
-  window.opener.spotifyCallback(token)
-}
-
-authenticateButton.onclick = function() {
-  let popup = window.open(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
-
-  window.spotifyCallback = (payload) => {
-    popup.close()
-
-    fetch('https://api.spotify.com/v1/me', {
-      headers: {
-        'Authorization': `Bearer ${payload}`
-      }
-    }).then(response => {
-      return response.json();
-    }).then(data => {
-      console.log(data);
-      document.getElementById("yourFollowers").innerHTML = "You have " + data.followers.total + " Followers";
-    })
-  }
+  // If we already have the token delete the authentication button
+  document.getElementById("authenticateButton").remove();
+  console.log(token);
+} else {
+  // If we don't have the token, add an authentication button to login to spotify.
+  document.getElementById("authenticateButton").onclick = function() {
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
+  };
 }
