06d32022ee580f9b1239b723ce5b6c9558187383
grantshandy <grantshandy@gmail.com>
2021-11-15 10:15:34 -0700

    now sorta understand async

--- index.js
+++ index.js
@@ -1,104 +1,53 @@
-import * as artists from "./artists.js";
-
 // Some settings for getting into spotify...
-const client_id = "2ed0e6e8b06842fb854cb15e1690a7b5";
+const client_id = '2ed0e6e8b06842fb854cb15e1690a7b5';
 const redirect_uri = window.location.href;
-const scopes = "user-follow-read";
+const scopes = 'user-follow-read';
 
-const width = 1280;
-const height = 720;
+// Get our token from the page URL
+var token = window.location.hash.substr(1).split('&')[0].split("=")[1]
 
-(async () => {
-  // Get our token from the page URL
-  var token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+// If the token isn't in the URL then try to get it from localStorage.
+if (token == null) {
+  token = localStorage.getItem('spotToken');
+}
 
-  // If the token isn't in the URL then try to get it from localStorage.
-  if (token == null) {
-    token = localStorage.getItem("spotToken");
-  }
-
-  // Check if our page has a token...
-  if (token) {
-    console.log("we have an auth_key!");
-    localStorage.setItem("spotToken", token);
-    await appLogic();
+// If we have a token...
+if (!token) {
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
+} else {
+  // Set our token in localstorage and start the app.
+  localStorage.setItem('spotToken', token);
   
-  } else {
-    console.log("no auth_key, forcing login.");
-
-    var authenticateButton = document.createElement("input");
-    authenticateButton.type = "button";
-    authenticateButton.value = "Login To Spotify";
-    authenticateButton.onclick = function() {
-      window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
-    };
-
-    document.body.appendChild(authenticateButton);
-
-  }
-})();
-
-// Here is the "real" entry point for our program. We should have our token at this point and can interact with the spotify api and write to the HTML DOM.
-async function appLogic() {
-  var links = new Array ();
-  var nodes = new Array ();
-  var ids = new Array ();
-
-  var nodes = getFollowingArtists();
-  nodes.forEach(logArtist);
-  // nodes.forEach(function(artist) {
-  //   console.log(artist);
-  //   // var relatedArtists = await artists.getRelatedArtists(artist.id);
-  //   // relatedArtists.forEach(function(related) {
-  //   //   if (!ids.includes(related.id)) {
-  //   //     links.push({ source: artist, target: related })
-  //   //     console.log("!");
-  //   //   }
-  //   // });
-  // });
-
-  // drawGraph(links, nodes);
+  getFollowing()
+    .then(connectArtists);
 }
 
-function logArtist(value) {
-  console.log(value);
-}
-
-function drawGraph(links, nodes) {
-  var graphDiv = document.createElement("div");
-  graphDiv.id = "3d-graph";
-  document.getElementById("content").appendChild(graphDiv);
-
-  const Graph = ForceGraph3D()
-  (document.getElementById('3d-graph'))
-    .graphData({ nodes, links })
-    .nodeLabel('name')
-    .width(width)
-    .height(height);
-
-  var logoutButton = document.createElement("input");
-  logoutButton.type = "button";
-  logoutButton.value = "Logout"
-  logoutButton.onclick = function() {
-    artists.logout();
-  }
-  document.body.appendChild(logoutButton);
-}
-
-async function getFollowingArtists() {
+// Get who we follow
+function getFollowing() {
   var artists = new Array ();
 
-  // Get our artists and print each of them.
-  await fetch(`https://api.spotify.com/v1/me/following?type=artist&limit=50`, {
-      headers: {
-      'Authorization': `Bearer ${localStorage.getItem("spotToken")}`,
-      }
-  }).then(response => response.json())
-  }).then(data => {
-      data.artists.items.forEach(function(artist) {
-          artists.push({ name: artist.name, id: artist.id });
-      });
-  });
+  return $.ajax({
+    url: 'https://api.spotify.com/v1/me/following?type=artist&limit=50',
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('spotToken')}`,
+    },
+  })
+}
 
-  return artists;
+// Get the related artists
+function connectArtists(response) {
+  if (!response.artists) {
+    resetToken();
+  }
+
+  console.log(response);
+}
+
+// If there's any problem getting data from spotify it probably means our token expired.
+// Here we reset the token by removing it from localStorage and reloading the page without the params.
+function resetToken() {
+  localStorage.removeItem('spotToken');
+  window.location.href = window.location.href.split('#')[0];
 }
