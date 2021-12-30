dc331c9cc36bf5ec3addaa04deebebc604b244df
grantshandy <grantshandy@gmail.com>
2021-12-29 20:16:38 -0700

    add a lot of stuff im too tired to explain.

--- index.js
+++ index.js
@@ -8,62 +8,73 @@ let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*' }));
+app.use(express.static('public'));
+// app.use(express.limit('4M'));
 
-app.post('/get', function (req, res) {
-    let code = req.body.code.toUpperCase();
+app.get('/data/:code', function (req, res) {
+    let code = req.params.code.toUpperCase().replace(/\s+/g, '');
 
     MongoClient.connect('mongodb://127.0.0.1:27017', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }, (err, client) => {
-
         if (err) {
-            res.end(err);
+            res.status(500).send(JSON.stringify(err));
+            return;
         }
 
         // Specify database you want to access
         const db = client.db('artist_constellations');
-
-        console.log(`finding ${code}`);
         db.collection("userData").find({ code: code }).toArray(function(err, result) {
             if (err) {
-                res.end(err);
+                res.status(500).send(JSON.stringify(err));
+                return;
             }
 
-            console.log(result);
-            res.end(JSON.stringify(result[0].data));
+            result = result[0]?.data ?? null;
+
+            if (result) {
+                res.status(200).send(JSON.stringify(result));
+            } else {
+                res.status(204).send(JSON.stringify({ error: "No result" }));
+            }
         });
     });
 });
 
-app.post('/send', function (req, res) {
+app.post('/upload', function (req, res) {
     let code = crypto.randomBytes(3).toString('hex').toUpperCase();
+
+    if (!req.body.user || !req.body.graphType || !req.body.nodes) {
+        console.log(req.body);
+        res.status(400).send(JSON.stringify({ error: "Invalid data"}));
+        return;
+    }
 
     MongoClient.connect('mongodb://127.0.0.1:27017', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }, (err, client) => {
-
         if (err) {
-            res.end(err);
+            res.status(500).send(JSON.stringify(err));
+            return;
         }
 
         const db = client.db('artist_constellations');
 
         db.collection('userData').insertOne({ code, data: req.body }, (err, result) => {
             if (err) {
-                res.end(err);
+                res.status(500).send(JSON.stringify(err));
+                return;
             }
         });
     });
 
-    res.end(code);
+    res.status(200).send(JSON.stringify({ code }));
 });
 
-let port = 8081;
+let port = 5500;
 
 console.log(`starting server on port ${port}`);
 let server = app.listen(port);
